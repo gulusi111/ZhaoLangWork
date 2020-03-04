103650ab22c9880175598bf3b7d6ae6a74dd7625
gulusi111 <420481657@qq.com>
2020-03-04 11:38:16 +0800

    Signed-off-by: gulusi111 <420481657@qq.com>

---
+++ ZhaoLang/assets/Scripts/Gamelayer.js
@@ -0,0 +1,26 @@
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+
+    },
+
+    onEnable() {
+        cc.log("onEnable");
+    },
+
+    start() {
+        cc.log("start");
+    },
+
+
+    onDestory() {
+        cc.log("onDestory");
+    },
+
+    onDisable() {
+        cc.log("onDisable");
+    },
+
+});
---
+++ ZhaoLang/assets/Scripts/player/PlayerNode.js
@@ -0,0 +1,20 @@
+
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        zy: 0,  //实际阵营
+        xzy:0,  //显示阵营
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
